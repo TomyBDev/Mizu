8649346936c3dc0d80f9380c669800900c4c380f
TomyBDev <ThomasBGameDev@gmail.com>
2023-02-11 16:53:48 +0000

    Logger now only logs in debug mode

--- Mizu/src/Tools/Log.h
+++ Mizu/src/Tools/Log.h
@@ -16,7 +16,17 @@ private:
 	static std::shared_ptr<spdlog::logger> logger;
 };
 
-#define LOG_ERROR(...) ::Log::GetLogger()->error(__VA_ARGS__)
-#define LOG_WARN(...) ::Log::GetLogger()->warn(__VA_ARGS__)
-#define LOG_INFO(...) ::Log::GetLogger()->info(__VA_ARGS__)
+#ifdef MZ_DEBUG
 #define LOG_TRACE(...) ::Log::GetLogger()->trace(__VA_ARGS__)
+#define LOG_INFO(...) ::Log::GetLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...) ::Log::GetLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...) ::Log::GetLogger()->error(__VA_ARGS__)
+#define LOG_FATAL(...) ::Log::GetLogger()->fatal(__VA_ARGS__)
+#else
+#define LOG_TRACE()
+#define LOG_INFO()
+#define LOG_WARN()
+#define LOG_ERROR()
+#define LOG_FATAL()
+#endif
+
